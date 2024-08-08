1c2ed35c0fa91f145d28ccbaddb57192ab919f53
CINDY SEPTIA TRIONITA <222111971@stis.ac.id>
2024-08-08 12:08:51 +0700

    pembaruan backuplaptop

--- app/src/main/java/com/uas/sipami/API/ApiClient.java
+++ app/src/main/java/com/uas/sipami/API/ApiClient.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.1.18:8083/";
+    private static final String BASE_URL = "http://10.135.16.105:8083/";
     private static Retrofit retrofit = null;
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
